43d50a3dbb5c812236d83474c023a819bc26935d
frog <lim47764909@gmail.com>
2024-09-30 00:45:55 +0900

    section 10

--- airclonebackend/categories/urls.py
+++ airclonebackend/categories/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Categories.as_view()), #class를 가져오기 위해 as_view() 사용
-    path("<int:pk>", views.CategoryDetail.as_view())
+    path("", views.CategoryViewSet.as_view(
+            {
+                "get" : "list",
+                "post" : "create"
+            }
+        ),
+    ), #class를 가져오기 위해 as_view() 사용
+    path("<int:pk>", views.CategoryViewSet.as_view(
+            {
+            "get" : "retrieve",
+            "put" : "partial_update",
+            "delete" : "destroy",
+            }
+        ),
+    ),
 ]
  
